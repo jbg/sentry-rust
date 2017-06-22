39a0c5eabcf3970335fbc08bb3a8749aa05ac88e
Jasper Bryant-Greene <jbg@rf.net.nz>
2017-06-22 11:10:27 +0100

    chrono 0.4 compat

--- src/lib.rs
+++ src/lib.rs
@@ -43,7 +43,7 @@ extern crate hyper_tls;
 use hyper_tls::HttpsConnector;
 
 extern crate chrono;
-use chrono::offset::utc::UTC;
+use chrono::offset::Utc;
 
 extern crate serde;
 #[macro_use]
@@ -190,7 +190,7 @@ impl Event {
         Event {
             event_id: "".to_string(),
             message: message.to_owned(),
-            timestamp: UTC::now().format("%Y-%m-%dT%H:%M:%S").to_string(), /* ISO 8601 format, without a timezone ex: "2011-05-02T17:41:36" */
+            timestamp: Utc::now().format("%Y-%m-%dT%H:%M:%S").to_string(), /* ISO 8601 format, without a timezone ex: "2011-05-02T17:41:36" */
             level: level.to_owned(),
             logger: logger.to_owned(),
             platform: "other".to_string(),
